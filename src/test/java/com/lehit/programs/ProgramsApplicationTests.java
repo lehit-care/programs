faaad4b435fe707aeac47f80600ec21d0c0f89f3
Ion Ionets <ion.ionets@agenatech.com>
2023-01-20 12:41:08 +0800

    Initial commit

---
+++ src/test/java/com/lehit/programs/ProgramsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lehit.programs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
